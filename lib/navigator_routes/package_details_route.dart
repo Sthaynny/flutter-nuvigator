26d9bc531ae9bef8471b41f9ed93cba3996cbfc2
isthaynny <igor.nascimento@popcode.com.br>
2021-10-05 20:11:32 -0300

    feat: add paramsParser in packege details

--- lib/navigator_routes/package_details_route.dart
+++ lib/navigator_routes/package_details_route.dart
@@ -1,17 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_nuvigator/models/package_model.dart';
+import 'package:flutter_nuvigator/models/producer_model.dart';
 import 'package:flutter_nuvigator/router/router.dart';
 import 'package:flutter_nuvigator/screens/package_details_screen.dart';
 import 'package:flutter_nuvigator/utils/string_export.dart';
 import 'package:nuvigator/next.dart';
 
-class PackageDetailsRoute extends NuRoute {
-  @override
-  Widget build(BuildContext context, NuRouteSettings<Object> settings) {
-    return PackageDetailsScreen(
-      package: settings.rawParameters[StringKeys.package],
-      producer: settings.rawParameters[StringKeys.producer],
+class PackageDatailsArgs {
+  final Producer producer;
+  final Package packege;
+
+  PackageDatailsArgs({
+    @required this.producer,
+    @required this.packege,
+  });
+
+  static PackageDatailsArgs fromJson(Map<String, dynamic> json) {
+    return PackageDatailsArgs(
+      packege: json[StringKeys.package],
+      producer: json[StringKeys.producer],
     );
   }
+}
+
+class PackageDetailsRoute
+    extends NuRoute<NuRouter, PackageDatailsArgs, String> {
+  @override
+  Widget build(
+      BuildContext context, NuRouteSettings<PackageDatailsArgs> settings) {
+    return PackageDetailsScreen(
+      package: settings.args.packege,
+      producer: settings.args.producer,
+    );
+  }
+
+  @override
+  ParamsParser<PackageDatailsArgs> get paramsParser =>
+      PackageDatailsArgs.fromJson;
 
   @override
   String get path => Routes.packageDetails;
